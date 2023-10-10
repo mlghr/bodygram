29e18659256cd1590f674c5a8ff07f7f2a3d5dad
Michael Rosenbaum <mrosenbaum.dev@gmail.com>
2023-10-09 21:13:36 -0700

    fill out generate organs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { initBody } from "./body-functions/initBody";
 import { Brain } from "./models/organs/Brain";
+import { Gallbladder } from "./models/organs/Gallbladder";
 import { Heart } from "./models/organs/Heart";
+import { Kidney } from "./models/organs/Kidney";
+import { Liver } from "./models/organs/Liver";
+import { Lung } from "./models/organs/Lung";
+import { Spleen } from "./models/organs/Spleen";
 
 function runBodySystems(): void {
   const body = initBody();
@@ -10,6 +15,11 @@ function runBodySystems(): void {
 function generateOrgans() {
   const heart = new Heart("full");
   const brain = new Brain();
+  const gallbladder = new Gallbladder();
+  const kidney = new Kidney();
+  const liver = new Liver();
+  const lung = new Lung();
+  const spleen = new Spleen();
 }
 
 generateOrgans();
